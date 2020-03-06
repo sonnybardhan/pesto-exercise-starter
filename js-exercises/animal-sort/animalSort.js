759a13fe6e34ddcb4ef7ffb0b6a0a81a2cbd8820
sonnybardhan <sonnybardhan@github.io>
2020-03-07 00:34:45 +0530

    solve: animals

--- js-exercises/animal-sort/animalSort.js
+++ js-exercises/animal-sort/animalSort.js
@@ -1,3 +1,13 @@
-const animalSort = animals => {};
+const animalSort = (animals) => {
+	animals.sort((prevAnimal, nextAnimal) => {
+		if (prevAnimal.numberOfLegs < nextAnimal.numberOfLegs) return -1;
+		if (prevAnimal.numberOfLegs > nextAnimal.numberOfLegs) return 1;
+		else {
+			if (prevAnimal.name < nextAnimal.name) return -1;
+			if (prevAnimal.name > nextAnimal.name) return 1;
+		}
+	});
+	return animals;
+};
 
 export { animalSort };
