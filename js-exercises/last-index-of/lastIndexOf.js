a9ccaea3332db7ff0d98a1f1e0c398b819207846
sonnybardhan <sonnybardhan@github.io>
2020-03-07 18:01:53 +0530

    solve: lastIndexOf

--- js-exercises/last-index-of/lastIndexOf.js
+++ js-exercises/last-index-of/lastIndexOf.js
@@ -1,8 +1,12 @@
-
 function lastIndexOf(...args) {
-  return args;
+	const item = args[0];
+	const list = args[1].reverse();
+	const index = list.indexOf(item);
+
+	if (index < 0) {
+		return -1;
+	}
+	return list.length - (index + 1);
 }
 
-export {
-  lastIndexOf,
-};
+export { lastIndexOf };
