b9a0989104e92fae7d202f75fe5d11bbb1575e81
sonnybardhan <sonnybardhan@github.io>
2020-03-07 17:59:57 +0530

    solve: deep-copy

--- js-exercises/deep-copy-object/deepCopyObject.js
+++ js-exercises/deep-copy-object/deepCopyObject.js
@@ -1,3 +1,19 @@
-const deepCopyObject = objToCopy => {};
+const deepCopyObject = (objToCopy) => {
+	if (objToCopy === null) return null;
+	if (typeof objToCopy !== 'object') return objToCopy;
+
+	const newObj = {};
+
+	for (let key in objToCopy) {
+		newObj[key] = typeof objToCopy[key] === 'object' ? deepCopyObject(objToCopy[key]) : objToCopy[key];
+	}
+	return newObj;
+};
 
 export { deepCopyObject };
+
+// if (typeof objToCopy[key] === 'object') {
+// 	newObj[key] = deepCopyObject(objToCopy[key]);
+// } else {
+// 	newObj[key] = objToCopy[key];
+// }
