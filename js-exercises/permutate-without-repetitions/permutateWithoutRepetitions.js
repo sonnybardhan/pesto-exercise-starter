b120feac3e15e4e43dadeac2cf062500855d5f08
sonnybardhan <sonnybardhan@github.io>
2020-03-12 15:52:14 +0530

    attempt permutations

--- js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.js
+++ js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -2,6 +2,18 @@
  * @param {*[]} permutationOptions
  * @return {*[]}
  */
-function permutateWithoutRepetitions(permutationOptions) {}
+function permutateWithoutRepetitions(options) {
+	const permutations = [];
+	permutations.push(options);
+
+	for (let i = 0; i < options.length; i++) {
+		let permutation = [];
+		for (j = 0; j < options.length; j++) {
+			//swap
+		}
+		permutations.push(permutation);
+	}
+	return permutations;
+}
 
 export { permutateWithoutRepetitions };
