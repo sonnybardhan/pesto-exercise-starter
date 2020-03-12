b9ef2ee9b0b9d22598e1fb13b4e4e49b388d04eb
sonnybardhan <sonnybardhan@github.io>
2020-03-12 18:29:42 +0530

    attempt permutations problem

--- js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.js
+++ js-exercises/permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,19 +1,26 @@
-/**
- * @param {*[]} permutationOptions
- * @return {*[]}
- */
-function permutateWithoutRepetitions(options) {
-	const permutations = [];
-	permutations.push(options);
+// /**
+//  * @param {*[]} permutationOptions
+//  * @return {*[]}
+//  */
+// function permutateWithoutRepetitions(options) {
+// 	const permutations = [];
+// 	permutations.push(options);
 
-	for (let i = 0; i < options.length; i++) {
-		let permutation = [];
-		for (j = 0; j < options.length; j++) {
-			//swap
-		}
-		permutations.push(permutation);
-	}
-	return permutations;
-}
+// 	for (let i = 0; i < options.length; i++) {
+// 		let permutation = [];
+// 		for (j = 0; j < options.length; j++) {
+// 			//swap
+// 		}
+// 		permutations.push(permutation);
+// 	}
+// 	return permutations;
+// }
+
+// function factorial(num) {
+// 	if (num === 0) return 1
+// 	if (num === 1) return 1
+// 	return num * factorial(num - 1);
+
+// }
 
 export { permutateWithoutRepetitions };
