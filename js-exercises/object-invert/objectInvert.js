03952eb5cabc1674eb38fa3adeb71fca03dfb66b
sonnybardhan <sonnybardhan@github.io>
2020-03-18 00:57:44 +0530

    solve object-invert

--- js-exercises/object-invert/objectInvert.js
+++ js-exercises/object-invert/objectInvert.js
@@ -1,8 +1,18 @@
-
-function objectInvert(...args) {
-  return args;
+function objectInvert(obj) {
+	const newKeys = Object.values(obj);
+	const newValues = Object.keys(obj);
+	const invertedObj = populateObj(newKeys, newValues);
+	return invertedObj;
 }
 
-export {
-  objectInvert,
-};
+function populateObj(newKeys, newValues) {
+	const obj = {};
+	for (let i = 0; i < newKeys.length; i++) {
+		const key = newKeys[i];
+		const value = newValues[i];
+		obj[key] = value;
+	}
+	return obj;
+}
+
+export { objectInvert };
