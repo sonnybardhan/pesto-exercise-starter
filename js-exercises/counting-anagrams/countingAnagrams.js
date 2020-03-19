200a7b56b893fd3f5b10651b5b6f43174b4e2006
sonnybardhan <sonnybardhan@github.io>
2020-03-20 00:21:16 +0530

    edit countingAnagrams

--- js-exercises/counting-anagrams/countingAnagrams.js
+++ js-exercises/counting-anagrams/countingAnagrams.js
@@ -3,15 +3,14 @@ const countingAnagrams = (str) => {
 	const strArray = str.map((word) => word.split('').sort().join('')).filter((word) => word.length > 1);
 
 	let count = 0;
-
 	const wordFrequencyMap = {};
 
 	for (let word of strArray) {
 		wordFrequencyMap[word] = (wordFrequencyMap[word] || 0) + 1;
 	}
 
-	for (let word in wordFrequencyMap) {
-		if (wordFrequencyMap[word] > 1) count += 1;
+	for (let word of Object.keys(wordFrequencyMap)) {
+		count += wordFrequencyMap[word] > 1 ? 1 : 0;
 	}
 
 	return count;
