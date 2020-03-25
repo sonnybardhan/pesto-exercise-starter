e38a963bcc23041741c9c8ad02bfb2bfc5eea55e
sonnybardhan <sonnybardhan@github.io>
2020-03-25 19:03:43 +0530

    solve questions for day14

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,12 +1,30 @@
-const allPromises = (args) => {
+//working example from tabrez
+
+const allPromises = (promises) => {
 	const results = [];
 	let completedPromises = 0;
 	return new Promise((resolve, reject) => {
-		//
-		//
-		//
-
-		resolve();
+		promises.forEach((promise, index) => {
+			if (promise instanceof Promise) {
+				promise
+					.then((value) => {
+						results[index] = value;
+						completedPromises += 1;
+						if (completedPromises === promises.length) {
+							resolve(results);
+						}
+					})
+					.catch((error) => {
+						reject(error);
+					});
+			} else {
+				results[index] = promise;
+				completedPromises += 1;
+				if (completedPromises === promises.length) {
+					resolve(results);
+				}
+			}
+		});
 	});
 };
 
