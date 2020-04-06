4f3ec2cebcd457c7959e3fd29a72bfa772db062d
sonnybardhan <sonnybardhan@github.io>
2020-04-06 15:31:27 +0530

    register keypress function

--- react-exercises/snake-game-react/src/GameMap.js
+++ react-exercises/snake-game-react/src/GameMap.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SnakeBody from './SnakeBody';
 import Food from './Food';
 import GridBlock from './GridBlock';
@@ -7,7 +7,12 @@ import GridBlock from './GridBlock';
 
 export default function() {
 	const grid = initializeMap();
-	
+	// console.log(grid[3][3]);
+	const snake = [{x: 3, y:3}];
+
+	placeSnake(grid, snake);
+
+	document.addEventListener('keydown', onInput);
 
 	return (
 		<div className="main-map">
@@ -16,6 +21,29 @@ export default function() {
 	)
 }
 
+function onInput({keyCode}){
+	switch(keyCode) {
+			case 37:
+				return console.log('left');
+				break;
+			case 38:
+				return console.log('up');
+				break;    
+			case 39:
+				return console.log('right');
+				break;
+			case 40:
+				return console.log('down');
+				break;
+			default:
+				break;            
+			}
+}
+
+function placeSnake(map, snake){
+	snake.forEach(({x, y}) => map[x][y] = <SnakeBody key={Math.random()}/>);
+	return map;
+}
 
 function initializeMap(){
 	const grid = [];
